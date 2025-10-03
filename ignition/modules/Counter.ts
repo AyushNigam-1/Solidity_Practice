a7ac8ed2384dd03a42ef2a203a6fe90dc0e8f234
Ayush <ayushnigam843@gmail.com>
2025-10-03 17:03:05 +0530

    Added ERC20 Contract (No Openzeppelin)

--- ignition/modules/Counter.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("ContractModule", (m) => {
-  const contract = m.contract("TipJar");
-
-  m.call(contract, "incBy", [5n]);
-
-  return { contract };
-});
